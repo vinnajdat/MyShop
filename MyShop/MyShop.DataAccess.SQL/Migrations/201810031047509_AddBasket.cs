d02234de5c558594cc766b6b3525f4e3bf36ba37
najdat nayef <vinnajdat@outlook.com>
2018-10-03 13:50:24 +0300

    Added Basket Model

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/201810031047509_AddBasket.cs
@@ -0,0 +1,43 @@
+namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        BasketID = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        Quanity = c.Int(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Baskets", t => t.BasketID)
+                .Index(t => t.BasketID);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "BasketID", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "BasketID" });
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
